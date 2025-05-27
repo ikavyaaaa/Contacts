ec6753fac6b5efe99e0a8b78d108a3487964a8db
Kavya Krishna <kavya.krishna@stryker.com>
2025-05-27 11:24:10 +0530

    Initial develop commit

---
+++ Contacts/ContactsTests/ContactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsTests.swift
+//  ContactsTests
+//
+//  Created by Kavya Krishna on 27/05/25.
+//
+
+import Testing
+@testable import Contacts
+
+struct ContactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
